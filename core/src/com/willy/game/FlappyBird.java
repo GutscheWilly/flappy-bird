d8394482c5f18c2234ce1932d9ae5b83bfad278d
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-01 00:17:08 -0300

    Class GameStateManager and FlappyBird render method

--- core/src/com/willy/game/FlappyBird.java
+++ core/src/com/willy/game/FlappyBird.java
@@ -1,9 +1,14 @@
 package com.willy.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.willy.game.States.GameStateManager;
+import com.willy.game.States.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 
@@ -11,21 +16,24 @@ public class FlappyBird extends ApplicationAdapter {
 	public static final int HEIGTH = 720;
 	public static final String TITLE = "Flappy Bird";
 
-	SpriteBatch batch;
+	private GameStateManager gameStateManager;
+	private SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		gameStateManager = new GameStateManager();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		ScreenUtils.clear(1, 0, 0, 1);
+		gameStateManager.push(new MenuState(gameStateManager));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gameStateManager.update(Gdx.graphics.getDeltaTime());
+		gameStateManager.render(batch);
 	}
 	
 	@Override
---
+++ core/src/com/willy/game/States/MenuState.java
@@ -0,0 +1,25 @@
+package com.willy.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(Float deltaTime) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch spriteBatch) {
+
+    }
+}
