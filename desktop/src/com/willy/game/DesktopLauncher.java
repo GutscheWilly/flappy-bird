2fa56ab9e95468dceec4760356b2f98fd8ee5a10
Willy Gutsche <willy.gutsche@unifesp.br>
2023-01-31 23:03:51 -0300

    Abstract class State and change config atributes

--- desktop/src/com/willy/game/DesktopLauncher.java
+++ desktop/src/com/willy/game/DesktopLauncher.java
@@ -4,12 +4,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.willy.game.FlappyBird;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Flappy Bird");
+		config.setWindowedMode(FlappyBird.WIDTH, FlappyBird.HEIGTH);
+		config.setTitle(FlappyBird.TITLE);
 		new Lwjgl3Application(new FlappyBird(), config);
 	}
 }
