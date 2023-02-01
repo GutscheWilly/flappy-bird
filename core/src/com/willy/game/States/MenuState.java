a6892e8ab19db35686faa7bf65b8e583c053985d
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-01 01:46:24 -0300

    Menu screen

--- core/src/com/willy/game/States/MenuState.java
+++ core/src/com/willy/game/States/MenuState.java
@@ -1,11 +1,18 @@
 package com.willy.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.willy.game.FlappyBird;
 
 public class MenuState extends State {
 
+    private Texture background;
+    private Texture playButton;
+
     public MenuState(GameStateManager gameStateManager) {
         super(gameStateManager);
+        background = new Texture("bg.png");
+        playButton = new Texture("playbtn.png");
     }
 
     @Override
@@ -20,6 +27,9 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-
+        spriteBatch.begin();
+        spriteBatch.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGTH);
+        spriteBatch.draw(playButton, (FlappyBird.WIDTH / 2) - (playButton.getWidth() / 2), FlappyBird.HEIGTH / 2);
+        spriteBatch.end();
     }
 }
